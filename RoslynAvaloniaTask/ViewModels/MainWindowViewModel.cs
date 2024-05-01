3d661b773acbd575e9586979ed005e363d2a9256
molczane <ernest.molczan@gmail.com>
2024-05-01 14:14:49 +0200

    Added basic traversing the Syntax Tree, a lot more to be done

--- RoslynAvaloniaTask/ViewModels/MainWindowViewModel.cs
+++ RoslynAvaloniaTask/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Runtime.InteropServices.ComTypes;
+using System.IO;
 using ReactiveUI;
 using System.Windows.Input;
+using Avalonia.Controls.Primitives;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -17,10 +18,16 @@ public class MainWindowViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _codeText, value);
     }
     private string _displayText;
-    public string DisplayText
+    private string DisplayText
     {
         get => _displayText;
         set => this.RaiseAndSetIfChanged(ref _displayText, value);
+    }
+    private string _syntaxTreeText;
+    public string SyntaxTreeText
+    {
+        get => _syntaxTreeText;
+        set => this.RaiseAndSetIfChanged(ref _syntaxTreeText, value);
     }
     public ICommand VisualizeCodeCommand { get; }
 #pragma warning disable CA1822 // Mark members as static
@@ -30,15 +37,73 @@ public class MainWindowViewModel : ViewModelBase
     {
         VisualizeCodeCommand = ReactiveCommand.Create(() =>
             {
-                Console.WriteLine(CodeText);
-                SyntaxTree tree = CSharpSyntaxTree.ParseText(CodeText);
-                CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
-                DisplayText = "";
-                DisplayText += $"The tree is a {root.Kind()} node.\n";
-                DisplayText += $"The tree has {root.Members.Count} elements in it.\n";
-                DisplayText += $"The tree has {root.Usings.Count} using statements. They are:\n";
-                foreach (UsingDirectiveSyntax element in root.Usings)
-                    DisplayText += $"\t{element.Name}\n";
+                try
+                {
+                    //Console.WriteLine(CodeText);
+                    SyntaxTree tree = CSharpSyntaxTree.ParseText(CodeText);
+                    CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+                    DisplayText = "";
+                    DisplayText += $"The tree is a {root.Kind()} node.\n";
+                    DisplayText += $"The tree has {root.Members.Count} elements in it.\n";
+                    DisplayText += $"The tree has {root.Usings.Count} using statements. They are:\n";
+                    foreach (UsingDirectiveSyntax element in root.Usings)
+                        DisplayText += $"\t{element.Name}\n";
+
+                    StringWriter stringWriter = new StringWriter();
+                    tree.GetRoot().WriteTo(stringWriter);
+
+                    SyntaxTreeText = "";
+                    // SyntaxTreeText += stringWriter.ToString();
+
+                    string rootString = root.ToString();
+                    var rootKind = root.Kind();
+                    var rootKindString = rootKind.ToString();
+                    
+                    var children = root.ChildNodesAndTokens();
+
+                    foreach (var child in children)
+                    {
+                        // Check if it's a node or token
+                        if (child.IsNode)
+                        {
+                            // If it's a node, you can cast it to SyntaxNode and further investigate
+                            SyntaxNode node = child.AsNode();
+                            SyntaxTreeText += $"Node: {node.Kind()}\n";
+                            Console.WriteLine($"Node: {node.Kind()}"); // Print the kind of the node
+
+                            // If you want to recursively explore the children of this node, you can do so
+                            // For example:
+                            foreach (var grandChild in node.ChildNodesAndTokens())
+                            {
+                                if (grandChild.IsNode)
+                                {
+                                    SyntaxNode grandChildNode = grandChild.AsNode();
+                                    SyntaxTreeText += $"  - Node: {grandChildNode.Kind()}\n";
+                                    Console.WriteLine(
+                                        $"  - Node: {grandChildNode.Kind()}"); // Print the kind of the grandchild node
+                                }
+                                else
+                                {
+                                    SyntaxToken grandChildToken = grandChild.AsToken();
+                                    SyntaxTreeText += $"  - Token: {grandChildToken.Kind()}\n";
+                                    Console.WriteLine(
+                                        $"  - Token: {grandChildToken.Kind()}"); // Print the kind of the grandchild token
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // If it's a token, you can cast it to SyntaxToken and further investigate
+                            SyntaxToken token = child.AsToken();
+                            SyntaxTreeText += $"Token: {token.Kind()}\n";
+                            Console.WriteLine($"Token: {token.Kind()}"); // Print the kind of the token
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    SyntaxTreeText = $"Error: {e.Message}";
+                }
             }
             );
     }
--- RoslynAvaloniaTask/obj/Debug/net8.0/RoslynAvaloniaTask.AssemblyInfo.cs
+++ RoslynAvaloniaTask/obj/Debug/net8.0/RoslynAvaloniaTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoslynAvaloniaTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792483bbd4a4f3b0a08bc526098a509b7c4578ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoslynAvaloniaTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoslynAvaloniaTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
